be26d664259a66dd2100fb6216cf1d4c321cd650
Thomas E Adams <tea3rd@gmail.com>
2021-03-04 17:42:26 -0500

    Vectorization changes with argparse correction adding provenance

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,3 +1,4 @@
+import pdb
 import argparse
 
 import xarray as xr
@@ -5,18 +6,19 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
 import cmocean
+import cmdline_provenance as cmdprov
 
 
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
-   
+    
     Args:
       darray (xarray.DataArray): Precipitation data
    
     """
    
     assert darray.units == 'kg m-2 s-1', "Program assumes input units are kg m-2 s-1"
-   
+
     darray.data = darray.data * 86400
     darray.attrs['units'] = 'mm/day'
    
@@ -25,22 +27,23 @@ def convert_pr_units(darray):
 
 def apply_mask(darray, sftlf_file, realm):
     """Mask ocean or land using a sftlf (land surface fraction) file.
-    
+   
     Args:
       darray (xarray.DataArray): Data to mask
       sftlf_file (str): Land surface fraction file
       realm (str): Realm to mask
-    
-    """
    
+    """
+  
     dset = xr.open_dataset(sftlf_file)
-    assert realm in ['land', 'ocean'], """Valid realms are 'land' or 'ocean'"""   
+    assert realm in ['land', 'ocean'], """Valid realms are 'land' or 'ocean'"""
     if realm == 'land':
         masked_darray = darray.where(dset['sftlf'].data < 50)
     else:
         masked_darray = darray.where(dset['sftlf'].data > 50)   
    
     return masked_darray
+
 
 def create_plot(clim, model, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
@@ -75,6 +78,31 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
     plt.title(title)
 
 
+def get_log_and_key(pr_file, history_attr, plot_type):
+    """Get key and command line log for image metadata.
+   
+    Different image formats allow different metadata keys.
+   
+    Args:
+      pr_file (str): Input precipitation file
+      history_attr (str): History attribute from pr_file
+      plot_type (str): File format for output image
+   
+    """
+    
+    valid_keys = {'png': 'History',
+                  'pdf': 'Title',
+                  'eps': 'Creator',
+                  'ps' : 'Creator'}    
+
+    assert plot_type in valid_keys.keys(), f"Image format not one of: {*[*valid_keys],}"
+    log_key = valid_keys[plot_type]
+    new_log = cmdprov.new_log(infile_history={pr_file: history_attr})
+    new_log = new_log.replace('\n', ' END ')
+    
+    return log_key, new_log
+
+
 def main(inargs):
     """Run the program."""
 
@@ -90,6 +118,11 @@ def main(inargs):
     create_plot(clim, dset.attrs['source_id'], inargs.season,
                 gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
+
+    log_key, new_log = get_log_and_key(inargs.pr_file,
+                                       dset.attrs['history'],
+                                       inargs.output_file.split('.')[-1])
+    plt.savefig(inargs.output_file, metadata={log_key: new_log}, dpi=200)
 
 
 if __name__ == '__main__':
