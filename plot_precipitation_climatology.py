7cb4e562649ed53bc50f75c6bd77fe1483ee8ccb
Thomas E Adams <tea3rd@gmail.com>
2021-03-04 16:14:03 -0500

    Vectorization changes

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,4 +1,3 @@
-import argparse
 import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
@@ -8,26 +7,46 @@ import cmocean
 
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
-   
+    
     Args:
       darray (xarray.DataArray): Precipitation data
-    
+   
     """
-    
+   
     darray.data = darray.data * 86400
     darray.attrs['units'] = 'mm/day'
    
     return darray
 
 
+def apply_mask(darray, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+   
+    Args:
+      darray (xarray.DataArray): Data to mask
+      sftlf_file (str): Land surface fraction file
+      realm (str): Realm to mask
+   
+    """
+  
+    dset = xr.open_dataset(sftlf_file)
+  
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data > 50)   
+   
+    return masked_darray
+
+
 def create_plot(clim, model, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
-   
+    
     Args:
       clim (xarray.DataArray): Precipitation climatology data
       model (str): Name of the climate model
       season (str): Season
-      
+     
     Kwargs:
       gridlines (bool): Select whether to plot gridlines
       levels (list): Tick marks on the colorbar    
@@ -36,7 +55,7 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
 
     if not levels:
         levels = np.arange(0, 13.5, 1.5)
-        
+       
     fig = plt.figure(figsize=[12,5])
     ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
     clim.sel(season=season).plot.contourf(ax=ax,
@@ -61,6 +80,10 @@ def main(inargs):
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
 
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
+
     create_plot(clim, dset.attrs['source_id'], inargs.season,
                 gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
@@ -78,7 +101,10 @@ if __name__ == '__main__':
                         help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
                         help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--mask", type=str, nargs=2,
+                        metavar=('SFTLF_FILE', 'REALM'), default=None,
+                        help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
 
     args = parser.parse_args()
-   
+  
     main(args)
